d43e3ffa735e967196221a49248c12da3858f209
Esteban Cervera <38500854+estebancervera@users.noreply.github.com>
2023-03-15 23:07:43 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyCam"
+    name: "SwiftyCam",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "SwiftyCam",
+            targets: ["SwiftyCam"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "6.6.0"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0")
+    ],
+    targets: [
+        .target(
+            name: "SwiftyCam",
+            dependencies: ["ReactiveSwift", "SwiftyJSON"],
+            path: "Sources"),
+        .testTarget(
+            name: "SwiftyCamTests",
+            dependencies: ["SwiftyCam"],
+            path: "Tests"),
+    ]
 )
